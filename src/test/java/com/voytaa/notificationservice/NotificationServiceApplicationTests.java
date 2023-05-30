b6256995942d69aadd78edd74eeedb88fa5f7ca7
Jan Wojtach <wojtachjanek@gmail.com>
2023-05-30 15:42:00 +0200

    Initial commit

---
+++ src/test/java/com/voytaa/notificationservice/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.voytaa.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
